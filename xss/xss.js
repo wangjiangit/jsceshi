ff0d10b4fed11255b263c2ecefbe4dd24cd7f686
wang jian <jiji_12342006@163.com>
2017-05-22 10:14:31 +0800

    添加xss测试

--- xss/xss.js
+++ xss/xss.js
@@ -2,8 +2,5 @@
  * Created by wj on 2017/5/17.
  */
 
-var temp=document.URL;
-var index=temp.indexOf('content=')+8;
-var uri=temp.substring(index);
-
-document.write(decodeURI(uri));
+alert('hello');
+alert('asdfsdf');
